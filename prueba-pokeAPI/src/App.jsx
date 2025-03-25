e3021d30ba7b79d8e5ea2dd79cf62f2528b4d074
Jean Paul <jgomez.fw@gmail.com>
2025-03-25 09:49:16 -0300

    se agrego capitalizacion para el nombre del pokemon, clase pokedexlogo

--- prueba-pokeAPI/src/App.jsx
+++ prueba-pokeAPI/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { getPokemon } from "../src/services/pokeApi";
+import "./App.css";
 
 function App() {
     const [pokemon, setPokemon] = useState(null);
@@ -11,22 +12,25 @@ function App() {
     };
 
     return (
-        <div>
-            <h1>Buscar Pokémon</h1>
+        <div className="main-pokedex">
+            <h2 className="pokedex-logo">Pokédex</h2>
             <input 
                 type="text" 
                 value={search} 
                 onChange={(e) => setSearch(e.target.value)} 
                 placeholder="Ingrese nombre o ID"
             />
-            <button onClick={handleSearch}>Buscar</button>
+            <button onClick={handleSearch} className="btn-buscar">Buscar</button>
 
             {pokemon && (
-                <div>
-                    <h2>{pokemon.name}</h2>
-                    <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-                    <p>Altura: {pokemon.height}</p>
-                    <p>Peso: {pokemon.weight}</p>
+                <div className="pokemon-info">
+                    <h2>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1).toLowerCase()}</h2>
+                    <h3>ID: {pokemon.id}</h3>
+                    <img 
+                        src={pokemon.sprites.front_default} 
+                        alt={pokemon.name} 
+                    />
+                    <p>Altura: {pokemon.height} Peso: {pokemon.weight}</p>
                 </div>
             )}
         </div>
